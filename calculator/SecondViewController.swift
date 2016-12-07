610ca090cc67242b102f0529ba8381a83e770658
石井真子 <mako@ishiimasako-no-macbook-air.local>
2016-12-07 18:35:06 +0900

    Initial Commit

---
+++ calculator/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  calculator
+//
+//  Created by 石井真子 on 2016/12/07.
+//  Copyright © 2016年 石井真子. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
